3f45fba9ba33df6153e489f81209311d7a4858fa
binaapp <chen.levami@gmail.com>
2025-06-03 15:35:58 +0300

    adding genious zone flow

--- src/composables/useFlowData.js
+++ src/composables/useFlowData.js
@@ -1,16 +1,12 @@
 import burnoutFlow from "@/data/flows/Burnout.json";
 import generalFlow from "@/data/flows/General.json";
 import quickCoachingFlow from "@/data/flows/QuickCoaching.json";
+import geniusZoneFlow from "@/data/flows/GeniousZone.json";
 // We can add more flows here if needed in the future
 // import generalFlow from '@/data/flows/General.json'
 
 // Define the default flow name as a constant
-export const DEFAULT_FLOW = "QuickCoaching";
-
-// Export the current active flow
-export const flowData = quickCoachingFlow;
-
-// If we need to switch flows in the future, we can add that functionality here
+export const DEFAULT_FLOW = "GeniusZone";
 
 // Returns the flow data based on the flowName parameter
 export function getFlowData(flowName = DEFAULT_FLOW) {
@@ -19,7 +15,11 @@ export function getFlowData(flowName = DEFAULT_FLOW) {
       return generalFlow;
     case "burnout":
       return burnoutFlow;
-    default:
+    case "geniuszone":
+      return geniusZoneFlow;
+    case "quickcoaching":
       return quickCoachingFlow;
+    default:
+      return geniusZoneFlow;
   }
 }
